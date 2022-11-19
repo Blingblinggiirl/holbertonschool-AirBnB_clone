ee9df28cb10a9b32e35d2cd9664508eff0aa8fb2
Nazarena <nazarenaaranda15@gmail.com>
2022-11-18 22:08:08 -0800

    7

--- console.py
+++ console.py
@@ -9,13 +9,15 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from models.user import User
 
 dictionary = {"Amenity": Amenity,
               "BaseModel": BaseModel,
               "City": City,
               "Place": Place,
               "Review": Review,
-              "State": State}
+              "State": State,
+              "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
